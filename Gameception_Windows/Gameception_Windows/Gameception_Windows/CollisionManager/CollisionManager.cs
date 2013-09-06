e57c0807c104cc32f232058edefb071dcd1a1baa
Merada <merada@yola.com>
2013-09-06 13:16:54 +0200

    Added collision manager file (empty)

---
+++ Gameception_Windows/Gameception_Windows/Gameception_Windows/CollisionManager/CollisionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gameception
+{
+    class CollisionManager
+    {
+        #region Attributes
+
+        
+
+        #endregion
+    }
+}
