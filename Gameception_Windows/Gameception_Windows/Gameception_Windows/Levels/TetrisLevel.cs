f43def4ad1f875998eb9bbdcdd0d0429da9d61f0
VictorSoudien <sdnvic001@myuct.ac.za>
2013-10-08 20:05:45 +0200

    Adjusted Player Speed

--- Gameception_Windows/Gameception_Windows/Gameception_Windows/Levels/TetrisLevel.cs
+++ Gameception_Windows/Gameception_Windows/Gameception_Windows/Levels/TetrisLevel.cs
@@ -82,12 +82,12 @@ namespace Gameception
             // player set up should move
             // also, perhaps two separate player objects for Player1 and NPC, inheriting from class Player,
             // to allow for weapon specialization
-            player1 = new Player(content.Load<Model>("Models/player1_model"), 0.1f, 100, new Vector3(5, 3.5f, 0), 3.5f, camera, PlayerIndex.One);
+            player1 = new Player(content.Load<Model>("Models/player1_model"), 0.3f, 100, new Vector3(5, 3.5f, 0), 3.5f, camera, PlayerIndex.One);
             player1.setKeys(Keys.W, Keys.D, Keys.S, Keys.A, Keys.Space, PlayerIndex.One);
             Weapon player1Weapon = new Weapon(20f, content.Load<Model>("Models/sphereHighPoly"), player1);
             player1.PlayerWeapon = player1Weapon;
 
-            player2 = new Player(content.Load<Model>("Models/npc_model"), 0.1f, 100, new Vector3(-5, 4f, 0), 0.6f, camera, PlayerIndex.Two);
+            player2 = new Player(content.Load<Model>("Models/npc_model"), 0.3f, 100, new Vector3(-5, 4f, 0), 0.6f, camera, PlayerIndex.Two);
             player2.setKeys(Keys.Up, Keys.Right, Keys.Down, Keys.Left, Keys.NumPad0, PlayerIndex.Two);
             Weapon player2Weapon = new Weapon(30f, content.Load<Model>("Models/sphereHighPoly"), player2);
             player2Weapon.ProjectileSpeed = 3f;
